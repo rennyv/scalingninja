78b0015b50439a3dd76d76bff7cbfa70679dce55
Renny Vanneste <renny@vanneste.ca>
2015-05-22 15:28:06 -0600

    character head and level schema filled in

--- models/character.js
+++ models/character.js
@@ -1,14 +1,30 @@
 var mongoose = require('mongoose');
 
 var levelSchema = mongoose.Schema({
+	baseattackbonus: Number,
+	fort: Number,
+	ref: Number,
+	will: Number,
+	hitdice: Number,
 	hitpoints: Number,
 	skillpoints: Number,
-	class: String
+	class: String,
+	levels: Number
 });
 
 var characterSchema = mongoose.Schema({
 	name:  String,
 	race: String,
+	size: String,
+	gender: String,
+	height: Number,
+	weight: String,
+	hair: String,
+	eyes: String,
+	skin: String,
+	age: Number,
+	alignment: String,
+	deity: String,
 	levels: [levelSchema]
 	
 });
