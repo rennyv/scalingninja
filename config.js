84060b2a1706a78300559d01a7e5dadfc2e82cce
Renny Vanneste <renny@vanneste.ca>
2015-05-22 15:56:47 -0600

    modified the hash

--- config.js
+++ config.js
@@ -2,6 +2,6 @@ var config = {};
 
 //need to add in path variables from Azure
 config.dbConnection =  process.env.CUSTOMCONNSTR_MONGOLAB_URI || 'mongodb://localhost:27017/try1';
-config.secretHash = 'super sercet hash';
+config.secretHash = process.env.SECRETHASH || 'super secret hash';
 	
 module.exports = config;
